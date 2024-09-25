1f7e139ffec3bec0326f9b3afe2363831cfe7d95
rwn <contact@0x57.me>
2024-09-25 20:06:33 +0200

    initial commit

---
+++ CM7/Debug/Drivers/BSP/Components/lan8742.d
@@ -0,0 +1,4 @@
+Drivers/BSP/Components/lan8742.o: \
+ /home/re/STM32CubeIDE/workspace_1.15.1/basestation-h7-netxduo/Drivers/BSP/Components/lan8742/lan8742.c \
+ /home/re/STM32CubeIDE/workspace_1.15.1/basestation-h7-netxduo/Drivers/BSP/Components/lan8742/lan8742.h
+/home/re/STM32CubeIDE/workspace_1.15.1/basestation-h7-netxduo/Drivers/BSP/Components/lan8742/lan8742.h:
